01222b159a1c7250df8b576d5e1aa21ad7443851
Shahar Yar <shaharyar.khan4511@gmail.com>
2025-10-24 23:35:42 -0700

    complete dashbarod add and for add product for my own store

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\AdminDashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/admin', function () {
+    return view('admin.dashboard');
+})->name('admin.dashboard');
+
+
+
+//admin dashbord
+Route::get('/admin',[AdminDashboardController::class, 'index']);
+
+
+
+
+
