aa04fff7e94e1e9247f2a4d44872e5689d0f90c2
Shahar Yar <shaharyar.khan4511@gmail.com>
2025-10-29 00:08:44 -0700

    update this new features add

--- app/Http/Controllers/Auth/UserAuthController.php
+++ app/Http/Controllers/Auth/UserAuthController.php
@@ -51,7 +51,7 @@ class UserAuthController extends Controller
     public function  login(Request $request)
     {
 
-        
+
         $credentials = $request->only('email', 'password');
 
         if (Auth::attempt($credentials)) {
